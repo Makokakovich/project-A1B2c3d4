724235ed48d49e9b638dc452e06ed0574907d6ba
Makokakovich <makokakovich@gmail.com>
2026-03-14 12:32:33 +0200

    feat: pip install підтримка, команда assistant

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+if sys.stdout.encoding.lower() != "utf-8":
+    sys.stdout.reconfigure(encoding="utf-8")
+
 from src.models import AddressBook, Record
 from src.models import NotesBook, Note
 from src.handlers.contact_handlers import (
